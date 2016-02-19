23a2d74d1b10eed9001d8b46ec8bc90504c41287
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2016-02-19 12:41:31 -0600

    get_insert_queries, prints replaced by returns

--- opinsert.py
+++ opinsert.py
@@ -7,6 +7,7 @@ from bson_processing import BsonProcessing
 
 
 def callback_internal(table, schema, data, fieldname):
+    res = []
     columns=[]
     #print 'callback', schema, data
     for type_item in schema:
@@ -23,23 +24,33 @@ def callback_internal(table, schema, data, fieldname):
                     values.append(record[type_item][dict_item])
             elif type(schema[type_item]) is not list:
                 values.append(str(record[type_item]))
-        print "INSERT INTO {table}({columns})\nVALUES({values});"\
-            .format(table=table, columns=','.join(columns), values=','.join(values))
+        res.append( "INSERT INTO {table}({columns})\nVALUES({values});"\
+                    .format(table=table, 
+                            columns=','.join(columns), 
+                            values=','.join(values)))
+    return res
 
 
-
-def opinsert_callback(ns, schema, objdata):
+def get_insert_queries(ns, schema, objdata):
     """ Get sql insert statements based on args.
     Args: 
     ns -- database.collection name
     schema -- json schema
     objdata - js data"""
 
-    bt = BsonProcessing(callback_internal)
+    res = []
+    bt = BsonProcessing(callback_internal, res)
     collection_name = ns.split('.')[1]
     tables = {}
-    #print collection_name, [schema], [objdata]
     bt.get_tables_structure([schema], [objdata], collection_name, "", tables)
+    return res
+
+
+def opinsert_callback(ns, schema, objdata):
+    return get_insert_queries(ns, schema, objdata)
+
+   
+
 
 
 if __name__ == "__main__":
--- oplog_handler.py
+++ oplog_handler.py
@@ -4,13 +4,19 @@ from oplog_parser import OplogParser
 from opinsert import opinsert_callback
 
 def default_cb_update(ns, schema, objdata, parent_id):
-    print "default update", parent_id, objdata
+    return ["default update", parent_id, objdata]
     
 def default_cb_delete(ns, schema, objdata):
-    print "default delete", objdata
+    return ["default delete", objdata]
 
 
 if __name__ == "__main__":
     p = OplogParser("./schemas", opinsert_callback, default_cb_update, default_cb_delete)
     with open("test_data/test_oplog.js", "r") as f:
-        p.parse(f)
+        p.load(f)
+        sqls = p.next()
+        while sqls:
+            print sqls
+            sqls = p.next()
+
+            
