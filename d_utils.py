b92069c8399fffaad9560f53805cc1c41046ab41
VarchukVladimir <fildes@gmail.com>
2016-03-07 12:33:23 -0600

    update removing end from parental collection names

--- d_utils.py
+++ d_utils.py
@@ -35,7 +35,7 @@ def get_table_name_from_list(spath):
     for it in spathl:
         if it.isdigit():
             spathl.remove(it)
-    if len(spath) > 1:
+    if len(spathl) > 1:
         return '_'.join(['_'.join((el[:-1]) for el in spathl[:-1]), spathl[-1]])
     else:
         return spathl[-1]
