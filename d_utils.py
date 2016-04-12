ea60e639efe8d7fe6adbff154f973bd810669e45
VarchukVladimir <fildes@gmail.com>
2016-04-12 15:47:51 -0500

    moving function for getting structure of tables to util module

--- d_utils.py
+++ d_utils.py
@@ -75,3 +75,84 @@ def get_last_idx_from_path(path):
         return spath[-1]
     else:
         return None
+
+
+def get_ids_list(lst):
+    if type(lst) is list:
+        list_it = lst[0]
+    else:
+        list_it = lst
+    ids_to_add = {}
+    for it in list_it:
+        if isIdField(it):
+            if type(list_it[it]) is dict:
+                for id_item in list_it[it]:
+                    if isIdField(id_item):
+                        ids_to_add[get_field_name_without_underscore(
+                            it + '_' + get_field_name_without_underscore(id_item))] = get_postgres_type(
+                            list_it[it][id_item])
+            else:
+                ids_to_add[get_field_name_without_underscore(it)] = get_postgres_type(list_it[it])
+    if len(ids_to_add) == 0:
+        ids_to_add['idx'] = 'bigint'
+    return ids_to_add
+
+
+def get_tables_structure(schema, table, table_mappings, parent_tables_ids, root_table):
+    if type(schema) is list:
+        table_struct = schema[0]
+    else:
+        table_struct = schema
+
+    table_mappings[table] = {}
+
+    for ids in parent_tables_ids:
+        table_mappings[table][ids] = parent_tables_ids[ids]
+
+    if not root_table:
+        table_mappings[table][u'idx'] = u'bigint'
+        parent_tables_ids[table + u'_idx'] = u'bigint'
+    else:
+        root_ids = get_ids_list(schema)
+        root_id_key = root_ids.iterkeys().next()
+        parent_tables_ids[table + '_' + root_id_key] = root_ids[root_id_key].decode('utf-8')
+    root_table = 0
+
+    if not type(table_struct) is dict:
+        table_mappings[table][u'data'] = table_struct
+        return table_mappings
+
+    for element in table_struct:
+        if type(table_struct[element]) is list:
+            get_tables_structure(table_struct[element], table[:-1] + '_' + get_field_name_without_underscore(element),
+                                 table_mappings, parent_tables_ids.copy(), root_table)
+        elif type(table_struct[element]) is dict:
+            get_table_struct_from_dict(table_struct[element], table, table_mappings, parent_tables_ids.copy(),
+                                       get_field_name_without_underscore(element))
+        else:
+            table_mappings[table][get_field_name_without_underscore(element)] = table_struct[element]
+    return table_mappings
+
+
+def get_table_struct_from_dict(schema, table, table_mappings, parent_tables_ids, parent_name):
+    for column in schema:
+        if type(schema[column]) is dict:
+            get_table_struct_from_dict(schema[column], table, table_mappings, parent_tables_ids,
+                                       parent_name + '_' + get_field_name_without_underscore(column))
+        elif type(schema[column]) is list:
+            get_tables_structure(schema[column], table[:-1] + '_' + parent_name + '_' + column, table_mappings,
+                                 parent_tables_ids, 0)
+        else:
+            table_mappings[table][parent_name + '_' + get_field_name_without_underscore(column)] = schema[column]
+
+
+def get_type(schema, table, field_name, collection_name):
+    return get_tables_structure(schema, collection_name, {}, {}, 1)[table][field_name]
+
+
+def get_quotes_using(schema, table, field_name, collection_name):
+    quotes_not_needed = ['int', 'bigint', 'integer']
+    if get_type(schema, table, field_name, collection_name) in quotes_not_needed:
+        return False
+    else:
+        return True
