5a1d7ca9614fdfab0ea5062a9646ef9411aaa1f0
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2016-04-13 03:56:27 +0000

    mongo_reader code style fixes

--- mongo_reader.py
+++ mongo_reader.py
@@ -8,7 +8,8 @@ __email__ = "yaroslav.litvinov@rackspace.com"
 
 import pprint
 import time
-import cProfile, pstats # profiling
+import cProfile
+import pstats  # profiling
 # modules affected in data in
 import sys
 import json
@@ -24,13 +25,16 @@ from gizer.opcreate import generate_drop_table_statement
 from gizer.opinsert import generate_insert_queries
 
 
-CSV_CHUNK_SIZE = 1024*1024*100 # 100MB
+CSV_CHUNK_SIZE = 1024 * 1024 * 100  # 100MB
+
 
 def message(mes, cr='\n'):
-    sys.stderr.write( mes + cr)
+    sys.stderr.write(mes + cr)
+
 
 class MongoReader:
-    def __init__(self, ssl, host, port, dbname, collection, \
+
+    def __init__(self, ssl, host, port, dbname, collection,
                  user, passw, request):
         self.ssl = ssl
         self.host = host
@@ -70,7 +74,8 @@ class MongoReader:
                 self.attempts += 1
                 if self.attempts <= 4:
                     time.sleep(pow(2, self.attempts))
-                    message("Connect attempt #%d, %s" % (self.attempts, str(time.time())))
+                    message("Connect attempt #%d, %s" %
+                            (self.attempts, str(time.time())))
                     continue
                 else:
                     self.failed = True
@@ -82,11 +87,12 @@ class MongoReader:
 
 
 def create_table(sqltable, psql_schema_name, psql_table_name_prefix):
-    drop = generate_drop_table_statement(sqltable, psql_schema_name, 
+    drop = generate_drop_table_statement(sqltable, psql_schema_name,
                                          psql_table_name_prefix)
-    create = generate_create_table_statement(sqltable, psql_schema_name, 
+    create = generate_create_table_statement(sqltable, psql_schema_name,
                                              psql_table_name_prefix)
-    return drop + '\n' + create + '\n';
+    return drop + '\n' + create + '\n'
+
 
 def merge_dicts(store, append):
     for index_key, index_val in append.iteritems():
@@ -104,7 +110,8 @@ def create_table_queries(sqltables, psql_schema_name, psql_table_name_prefix):
     for tablename, sqltable in sqltables.iteritems():
         if tablename not in create_table_queries.created_tables:
             create_table_queries.created_tables[tablename] = \
-                create_table(sqltable, psql_schema_name, psql_table_name_prefix)
+                create_table(sqltable, psql_schema_name,
+                             psql_table_name_prefix)
 
 
 def gen_insert_queries(tables_obj, csm):
@@ -117,18 +124,21 @@ def gen_insert_queries(tables_obj, csm):
         written[name] = reccount
     return written
 
-#cache initial indexes
+# cache initial indexes
     gen_insert_queries.max_indexes = \
-        merge_dicts(gen_insert_queries.max_indexes, tables_obj.data_engine.indexes)
+        merge_dicts(gen_insert_queries.max_indexes,
+                    tables_obj.data_engine.indexes)
 
 
 if __name__ == "__main__":
-    
+
     default_request = '{}'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-ssl", action="store_true", help="connect to ssl port")
-    parser.add_argument("--host", help="Mongo db host:port", type=str, required=True)
+    parser.add_argument("-ssl", action="store_true",
+                        help="connect to ssl port")
+    parser.add_argument("--host", help="Mongo db host:port",
+                        type=str, required=True)
     parser.add_argument("-user", help="Mongo db user", type=str)
     parser.add_argument("-passw", help="Mongo db pass", type=str)
     parser.add_argument("-cn", "--collection-name", help="Mongo collection name that is \
@@ -141,11 +151,12 @@ default=%s' % (default_request), type=str)
     parser.add_argument("-psql-table-name-prefix", help="", type=str)
     parser.add_argument("--ddl-statements-file", help="File to save create table \
 statements", type=argparse.FileType('w'), required=True)
-    parser.add_argument("-stats-file", help="File to write written record counts", 
+    parser.add_argument("-stats-file", help="File to write written record counts",
                         type=argparse.FileType('w'))
     parser.add_argument("--hdfs-path", help="Hdfs path (at least 3 letters) to save \
 folders with csv files", type=str, required=True)
-    parser.add_argument("--csv-path", help="base path for results", type=str, required=True)
+    parser.add_argument(
+        "--csv-path", help="base path for results", type=str, required=True)
 
     args = parser.parse_args()
 
@@ -158,7 +169,7 @@ folders with csv files", type=str, required=True)
         message("collection name is expected in format db_name.collection_name")
         exit(1)
 
-    message("Connecting to mongo server "+args.host)
+    message("Connecting to mongo server " + args.host)
     split_host = args.host.split(':')
     if len(split_host) > 1:
         host = split_host[0]
@@ -173,16 +184,16 @@ folders with csv files", type=str, required=True)
     if args.js_request is None:
         args.js_request = default_request
 
-    pr = cProfile.Profile() #profiling
-    pr.enable() #profiling
+    pr = cProfile.Profile()  # profiling
+    pr.enable()  # profiling
 
     search_request = json.loads(args.js_request)
 
     js_schema = [json.load(args.input_file_schema)]
     schema = schema_engine.SchemaEngine(collection_name, js_schema)
-    mongo_reader = MongoReader(args.ssl, host, port, \
-                                   dbname, collection_name, \
-                                   args.user, args.passw, search_request)
+    mongo_reader = MongoReader(args.ssl, host, port,
+                               dbname, collection_name,
+                               args.user, args.passw, search_request)
 
     psql_schema_name = args.psql_schema_name
     if not args.psql_schema_name:
@@ -195,7 +206,8 @@ folders with csv files", type=str, required=True)
     sqltables = schema_engine.create_tables_load_bson_data(schema, None).tables
     table_names = sqltables.keys()
 
-    csm = CsvManager(table_names, args.csv_path, args.hdfs_path, CSV_CHUNK_SIZE)
+    csm = CsvManager(table_names, args.csv_path,
+                     args.hdfs_path, CSV_CHUNK_SIZE)
     pp = pprint.PrettyPrinter(indent=4)
     errors = {}
     all_wrtitten_reccount = {}
@@ -207,10 +219,13 @@ folders with csv files", type=str, required=True)
             if count is None:
                 count = mongo_reader.cursor.count()
             if rec:
-                tables_obj = schema_engine.create_tables_load_bson_data(schema, [rec])
-                create_table_queries(tables_obj.tables, psql_schema_name, psql_table_name_prefix)
+                tables_obj = schema_engine.create_tables_load_bson_data(schema, 
+                                                                        [rec])
+                create_table_queries(
+                    tables_obj.tables, psql_schema_name, psql_table_name_prefix)
                 written = gen_insert_queries(tables_obj, csm)
-                all_wrtitten_reccount = merge_dicts(all_wrtitten_reccount, written)
+                all_wrtitten_reccount = merge_dicts(
+                    all_wrtitten_reccount, written)
                 errors = merge_dicts(errors, tables_obj.errors)
                 message(".", cr="")
                 if mongo_reader.rec_i % 1000 == 0:
@@ -218,21 +233,21 @@ folders with csv files", type=str, required=True)
     except KeyboardInterrupt:
         mongo_reader.failed = True
 
-#save create table statements    
+# save create table statements
     for table_name, create_query in \
             create_table_queries.created_tables.iteritems():
         args.ddl_statements_file.write(create_query)
-#save csv files
+# save csv files
     csm.finalize()
     message("")
     pr.disable()
-    ps = pstats.Stats(pr).sort_stats('cumulative') #profiling
+    ps = pstats.Stats(pr).sort_stats('cumulative')  # profiling
     ps.print_stats()
 
     pp.pprint(errors)
     pp.pprint(all_wrtitten_reccount)
     if args.stats_file:
         for name, value in all_wrtitten_reccount.iteritems():
-            args.stats_file.write(name+" "+str(value)+"\n")
+            args.stats_file.write(name + " " + str(value) + "\n")
 
     exit(mongo_reader.failed)
