b8137023b1a47b993ec49de930f92fbab4d2cf65
VarchukVladimir <fildes@gmail.com>
2016-04-20 14:01:53 -0500

    fix tests after function renaming, update tests for function "update"

--- test_update.py
+++ test_update.py
@@ -27,23 +27,50 @@ def test_get_obj_id_recursive():
     print(TEST_INFO, 'get_obj_id_recursive', 'PASSED')
 
 
-def test_update_new():
-    print('######################')
-    print(update_new(schema, oplog_u_01))
-    print('######################')
-    print(update_new(schema, oplog_u_02))
-    print('######################')
-    print(update_new(schema, oplog_u_03))
-    print('######################')
-    print(update_new(schema, oplog_u_04))
-    print('######################')
-    print(update_new(schema, oplog_u_05))
-    print('######################')
-    print(update_new(schema, oplog_u_06))
-    print('######################')
-    print(update_new(schema, oplog_u_07))
-    print('######################')
-    print(update_new(schema, oplog_u_08))
+def test_update():
+
+    oplog_data = oplog_u_01
+    model = [{'UPDATE post_comments SET body=%s, created_at=%s, id_oid=%s, updated_at=%s WHERE posts_id_oid=%s and idx=%s;': ['comment6', '2016-02-08T19:42:33.589Z', '56b8efa9f9fcee1b0000000f', '2016-02-08T19:42:33.589Z', '56b8da51f9fcee1b00000006', '5']}, {'INSERT INTO post_comments (body, created_at, id_oid, updated_at) VALUES(%s, %s, %s, %s)': ['comment6', '2016-02-08T19:42:33.589Z', '56b8efa9f9fcee1b0000000f', '2016-02-08T19:42:33.589Z', '56b8da51f9fcee1b00000006', '5']}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_02
+    model = [{'UPDATE posts SET updated_at=%s WHERE id_oid=%s;': ['2016-02-08T19:52:23.883Z', '56b8da59f9fcee1b00000007']}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_03
+    model = [{'DELETE FROM post_comments WHERE (id_oid="%s");': ['56b8da59f9fcee1b00000007']}, {'INSERT INTO {table} ({columns}) VALUES({values})': []}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_04
+    model = [{'UPDATE post_comments SET body=%s, created_at=%s, id_oid=%s, updated_at=%s WHERE posts_id_oid=%s and idx=%s;': ['commments2222', '2016-02-08T19:58:06.008Z', '56b8f34ef9fcee1b00000019', '2016-02-08T19:58:06.008Z', '56b8da59f9fcee1b00000007', '1']}, {'INSERT INTO post_comments (body, created_at, id_oid, updated_at) VALUES(%s, %s, %s, %s)': ['commments2222', '2016-02-08T19:58:06.008Z', '56b8f34ef9fcee1b00000019', '2016-02-08T19:58:06.008Z', '56b8da59f9fcee1b00000007', '1']}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_05
+    model = [{'UPDATE post_comments SET created_at=%s, id_oid=%s, updated_at=%s WHERE posts_id_oid=%s and idx=%s;': ['2016-02-08T19:58:22.847Z', '56b8f35ef9fcee1b0000001a', '2016-02-08T19:58:22.847Z', '56b8da59f9fcee1b00000007', '2']}, {'INSERT INTO post_comments (created_at, id_oid, updated_at) VALUES(%s, %s, %s)': ['2016-02-08T19:58:22.847Z', '56b8f35ef9fcee1b0000001a', '2016-02-08T19:58:22.847Z', '56b8da59f9fcee1b00000007', '2']}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_06
+    model = [{'DELETE FROM post_comments WHERE (id_oid="%s");': ['56b8da59f9fcee1b00000007']}, {'INSERT INTO {table} ({columns}) VALUES({values})': []}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_07
+    model = [{'DELETE FROM post_comments WHERE (id_oid="%s");': ['56b8da59f9fcee1b00000007']}, {'INSERT INTO {table} ({columns}) VALUES({values})': []}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+    oplog_data = oplog_u_08
+    model = [{'UPDATE posts SET updated_at=%s, title=%s WHERE id_oid=%s;': ['2016-02-08T20:02:12.985Z', 'sada', '56b8f05cf9fcee1b00000010']}]
+    result = update(schema, oplog_data)
+    assert result == model
+
+
+    print(TEST_INFO, 'get_obj_id', 'PASSED')
 
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -51,4 +78,4 @@ pp = pprint.PrettyPrinter(indent=4)
 
 test_get_obj_id()
 test_get_obj_id_recursive()
-test_update_new()
+test_update()
