a190b53465623a9ae420a54efa628f5bc009f5be
VarchukVladimir <fildes@gmail.com>
2016-04-22 19:38:46 -0500

    update parental idx column name with ful name of all parental tables

--- opdelete.py
+++ opdelete.py
@@ -51,11 +51,13 @@ def get_tables_list(schema, table):
 def get_conditions_list(schema, path, id):
     spath = path.split('.')
     parental_tables_list = get_indexes_dictionary(path)
+    parental_tables_idx_list = get_indexes_dictionary_idx(path)
     target_table = get_table_name_from_list(spath)
+    target_table_idxname_for_child = get_idx_column_name_from_list(spath)
     params_target = {}
-    for it in parental_tables_list:
-        if target_table <> it:
-            params_target[it + '_idx'] = parental_tables_list[it]
+    for it in parental_tables_idx_list:
+        if target_table_idxname_for_child <> it:
+            params_target[it + '_idx'] = parental_tables_idx_list[it]
 
     ids = get_ids_list(schema)
     root_id = ids.iterkeys().next()
@@ -65,13 +67,13 @@ def get_conditions_list(schema, path, id):
 
     if root_table == target_table:
         params_target[root_id] = id
-        params_child[target_table + '_' + root_id] = id
+        params_child[target_table_idxname_for_child + '_' + root_id] = id
     else:
         params_target[root_table + '_' + root_id] = id
         params_child[root_table + '_' + root_id] = id
-        if target_table in parental_tables_list.keys():
-            params_target['idx'] = parental_tables_list[target_table]
-            params_child[target_table + '_idx'] = parental_tables_list[target_table]
+        if target_table_idxname_for_child in parental_tables_idx_list.keys():
+            params_target['idx'] = parental_tables_idx_list[target_table_idxname_for_child]
+            params_child[target_table_idxname_for_child + '_idx'] = parental_tables_idx_list[target_table_idxname_for_child]
     return {'target': params_target, 'child': params_child}
 
 
@@ -86,10 +88,11 @@ def get_where_templates(conditions_list):
         return temp
 
     where_list = {'target': {}, 'child': {}}
-    where_list['target']['template'] = ' and '.join([(condition_with_quotes(key)) for key in conditions_list['target']])
-    where_list['target']['values'] = [conditions_list['target'][key] for key in conditions_list['target']]
-    where_list['child']['template'] = ' and '.join([(condition_with_quotes(key)) for key in conditions_list['child']])
-    where_list['child']['values'] = [conditions_list['child'][key] for key in conditions_list['child']]
+    where_list['target']['template'] = ' and '.join(sorted([(condition_with_quotes(key)) for key in conditions_list['target']]))
+    where_list['target']['values'] = [conditions_list['target'][key] for key in sorted(conditions_list['target'])]
+    where_list['child']['template'] = ' and '.join(sorted([(condition_with_quotes(key)) for key in conditions_list['child']]))
+    where_list['child']['values'] = [conditions_list['child'][key] for key in sorted(conditions_list['child'])]
+
     return where_list
 
 
@@ -97,6 +100,7 @@ def gen_statements(schema, path, id):
     where_clauses = get_where_templates(get_conditions_list(schema, path, id))
     all_tables_list = get_tables_list(schema, get_root_table_from_path(path))
     target_table = get_table_name_from_list(path.split('.'))
+    target_table_idx_name = get_idx_column_name_from_list (path.split('.'))
     tables_list = []
     for table in all_tables_list:
         if str.startswith(str(table), target_table[:-1], 0, len(table)) and not table == target_table:
@@ -124,7 +128,7 @@ def gen_statements(schema, path, id):
             'values']
 
         for table in tables_list:
-            update_statements[UPDATE_TMPLT.format(table=table, statements=target_table + '_idx=' + str(ind - 1),
+            update_statements[UPDATE_TMPLT.format(table=table, statements=target_table_idx_name + '_idx=' + str(ind - 1),
                                                  conditions=udpate_where['child']['template'])] = udpate_where['child'][
                 'values']
     return {'del': del_statements, 'upd': update_statements}
