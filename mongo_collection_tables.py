8cea0a9f6461e4e3c1ce370bbe8052a1b9dc32ab
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2016-05-11 11:30:40 -0500

    codestyle fixes

--- mongo_collection_tables.py
+++ mongo_collection_tables.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
+""" Get list of psql tables corresponding to specified collection. """
+
 __author__ = "Yaroslav Litvinov"
 __copyright__ = "Copyright 2016, Rackspace Inc."
 __email__ = "yaroslav.litvinov@rackspace.com"
 
-"""Read data from mongo collection"""
-
-import sys
 import argparse
 import json
 from mongo_schema import schema_engine
 
-def message(mes, cr='\n'):
-    sys.stderr.write( mes + cr)
-
-if __name__ == "__main__":
-    
-    default_request = '{}'
-
+def main():
+    """ main """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-cn", "--collection-name", help="Mongo collection name", 
+    parser.add_argument("-cn", "--collection-name",
+                        help="Mongo collection name",
                         type=str, required=True)
     parser.add_argument("-ifs", "--input-file-schema", action="store",
-                        help="Input file with json schema", type=file, required=True)
-    parser.add_argument("-delim", help="Use delimiter between table name components", 
+                        help="Input file with json schema",
+                        type=file, required=True)
+    parser.add_argument("-delim",
+                        help="Use delimiter between table name components",
                         type=str, required=True)
 
     args = parser.parse_args()
@@ -34,3 +31,6 @@ if __name__ == "__main__":
     schema = schema_engine.SchemaEngine(collection_name, js_schema)
     for i in schema.get_tables_list(args.delim):
         print i
+
+if __name__ == "__main__":
+    main()
