186d49180e23dbf994ff3c49d3a143025670a1cc
VarchukVladimir <fildes@gmail.com>
2016-05-17 13:10:57 -0500

    update test data

--- test_data/posts_data_target_oplog_sync.js
+++ test_data/posts_data_target_oplog_sync.js
@@ -1,21 +1,26 @@
 [
  {
      "_id": { "$oid": "56b8f05cf9fcee1b00000010" },
-     "title": "title3",
+     "title": "sada",
      "body": "body3",
      "user_id": "56b8d7caf9fcee1b00000001",
-     "updated_at": { "$date" : "2016-02-08T19:45:32.501Z"},
+     "updated_at": { "$date" : "2016-02-08T20:02:12.985Z"},
      "created_at": { "$date" : "2016-02-08T19:45:32.501Z"}
  },
  {
      "_id": { "$oid": "56b8da59f9fcee1b00000007" },
-     "comments": [{
-             "_id": { "$oid": "56b8f344f9fcee1b00000018" },
-             "updated_at": {"$date": "2016-02-08T19:57:56.678Z"},
-             "created_at": {"$date": "2016-02-08T19:57:56.678Z"}
-      },
-      {
-          "tests": [101]
-      }]
+     "updated_at" : { "$date" :"2016-02-08T19:52:23.883Z"},
+     "comments": [
+        {
+          "_id": { "$oid": "56b8f344f9fcee1b00000018" },
+          "updated_at": "2016-02-08T19:57:56.678Z",
+          "created_at": "2016-02-08T19:57:56.678Z"
+        },
+        {
+          "_id": { "$oid": "56b8f35ef9fcee1b0000001a" },
+          "updated_at": "2016-02-08T19:58:22.847Z",
+          "created_at": "2016-02-08T19:58:22.847Z"
+        }
+     ]
  }
 ]
