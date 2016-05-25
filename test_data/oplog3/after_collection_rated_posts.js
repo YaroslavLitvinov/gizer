c84dbe0ba1f444fcfaeb046c3d47fab5d60e37d0
VarchukVladimir <fildes@gmail.com>
2016-05-25 17:00:32 -0500

    add new collection for tests

---
+++ test_data/oplog3/after_collection_rated_posts.js
@@ -0,0 +1,70 @@
+[
+{
+    "_id": { "$oid": "56b8f05cf9fcee1b00001000" },
+    "title": "The Gaslo!",
+    "body": "Glory for Ukraine! Glory for Heroes!",
+    "user_id": "56b8d7caf9fcee1b00000001",
+    "updated_at": { "$date" : "2016-02-08T19:01:00.000Z"},
+    "created_at": { "$date" : "2016-02-08T19:00:00.000Z"},
+	"tests": [10,20,30],
+    "comments": [
+		{
+			"_id": { "$oid": "56b8f05cf9fcee1b00002000" },
+			"user_id" : "56b8d7caf9fcee1b00000002",
+	        "body": "Good post!",
+		    "updated_at": { "$date" : "2016-02-08T19:01:00.000Z"},
+		    "created_at": { "$date" : "2016-02-08T19:01:00.000Z"},
+	        "tests" : [ 1,2,3 ],
+			"rates" : [
+				{
+					"_id": { "$oid": "56b8f05cf9fcee1b00003000" ,"bsontype": 3},
+					"user_id" : "56b8d7caf9fcee1b00000003",
+					"rate" : 10,
+				    "updated_at": { "$date" : "2016-02-08T19:02:00.000Z"},
+				    "created_at": { "$date" : "2016-02-08T19:02:00.000Z"}
+				},
+				{
+					"_id": { "$oid": "56b8f05cf9fcee1b00003001" ,"bsontype": 3},
+					"user_id" : "56b8d7caf9fcee1b00000004",
+					"rate" : 158,
+				    "updated_at": { "$date" : "2016-02-08T19:05:00.000Z"},
+				    "created_at": { "$date" : "2016-02-08T19:03:00.000Z"}
+				}
+			]
+    	},
+		{
+			"_id": { "$oid": "56b8f05cf9fcee1b00002001" },
+			"user_id" : "56b8d7caf9fcee1b00000002",
+	        "body": "Good post!",
+		    "updated_at": { "$date" : "2016-02-08T19:08:00.000Z"},
+		    "created_at": { "$date" : "2016-02-08T19:08:00.000Z"},
+	        "tests" : [ 1,2,3 ],
+			"rates" : [
+				{
+					"_id": { "$oid": "56b8f05cf9fcee1b00003004" ,"bsontype": 3},
+					"user_id" : "56b8d7caf9fcee1b00000003",
+					"rate" : 10,
+				    "updated_at": { "$date" : "2016-02-08T19:09:00.000Z"},
+				    "created_at": { "$date" : "2016-02-08T19:09:00.000Z"}
+				},
+				{
+					"_id": { "$oid": "56b8f05cf9fcee1b00003005" ,"bsontype": 3},
+					"user_id" : "56b8d7caf9fcee1b00000004",
+					"rate" : 100,
+				    "updated_at": { "$date" : "2016-02-08T19:09:00.000Z"},
+				    "created_at": { "$date" : "2016-02-08T19:09:00.000Z"}
+				}
+			]
+    	}
+	],
+	"rates": [
+		{
+			"_id": { "$oid": "56b8f05cf9fcee1b00002000" },
+			"user_id" : "56b8d7caf9fcee1b00000002",
+			"rate" : 10,
+		    "created_at": { "$date" : "2016-02-10T19:00:30.000Z"},
+		    "updated_at": { "$date" : "2016-02-10T19:00:30.000Z"}	
+		}
+	]
+}
+]
