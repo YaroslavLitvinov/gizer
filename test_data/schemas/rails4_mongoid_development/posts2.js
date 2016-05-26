eceb99a1e8f4080120886a2d154895bb40f5a6e1
VarchukVladimir <fildes@gmail.com>
2016-05-26 18:19:38 -0500

    update tests

---
+++ test_data/schemas/rails4_mongoid_development/posts2.js
@@ -0,0 +1,21 @@
+{
+    "comments": [{
+        "_id": {
+            "oid": "STRING",
+            "bsontype": "INT"
+        },
+        "body": "STRING",
+        "updated_at": "TIMESTAMP",
+        "created_at": "TIMESTAMP",
+        "tests" : [ "INT" ]
+    }],
+    "title": "STRING",
+    "body": "STRING",
+    "user_id": "STRING",
+    "updated_at": "TIMESTAMP",
+    "created_at": "TIMESTAMP",
+    "_id": {
+        "oid": "STRING",
+        "bsontype": "INT"
+    }
+}
