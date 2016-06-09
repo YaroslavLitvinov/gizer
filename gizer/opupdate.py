f551cf0352808bbd2cf3e528e1865fa266289ad4
VarchukVladimir <fildes@gmail.com>
2016-06-09 15:01:34 -0500

    added multi set for UPDATE operation

--- gizer/opupdate.py
+++ gizer/opupdate.py
@@ -30,92 +30,185 @@ import datetime
 # """
 
 
-def update (dbreq, schema_e, oplog_data, database_name, schema_name):
+def localte_in_schema(schema_in, path):
+    if type(schema_in) is list:
+        schema = schema_in[0]
+    else:
+        schema = schema_in
+    new_path_clear = []
+    for el in path:
+        if not el.isdigit():
+            new_path_clear.append(el)
+
+    current_element = new_path_clear[0]
+    if current_element in schema.keys():
+        if len(new_path_clear) > 1:
+            new_path = new_path_clear[1:]
+            if type(schema[current_element]) is list:
+                next_element = schema[current_element][0]
+            elif type(schema[current_element]) is dict:
+                next_element = schema[current_element]
+
+            if len(new_path) >= 1:
+                return localte_in_schema(next_element, new_path)
+            else:
+                if new_path in next_element.keys():
+                    return  True
+                else:
+                    return False
+        else:
+            return True
+    else:
+        return False
+
+
+def update(dbreq, schema_e, oplog_data, database_name, schema_name):
+    if type(schema_e) != dict:
+        schema = schema_e.schema
+    else:
+        schema = schema_e
+    oplog_data_object_id = oplog_data['o2']
+    oplog_data_ns = oplog_data['ns']
+    oplog_data_set = oplog_data['o']['$set']
+    root_table_name = oplog_data_ns.split('.')[-1]
+    tables_mappings = get_tables_structure(schema, root_table_name, {}, {}, '')
+    ret_val = []
+    is_root_object_updated = False
+    for element in oplog_data_set:
+        updating_obj = element.split('.')
+        upd_path = [root_table_name] + updating_obj
+        if not localte_in_schema(schema[0], updating_obj):
+            continue
+
+        if len(updating_obj) > 1:
+            if updating_obj[-1].isdigit():
+                ret_val.extend(update_cmd(dbreq,schema_e,oplog_data_set,oplog_data_object_id,oplog_data_ns,tables_mappings,database_name,schema_name))
+            else:
+                new_oplog_data_set = { '.'.join(updating_obj[:-1]):{updating_obj[-1]:oplog_data_set[element]}}
+                ret_val.extend(update_cmd(dbreq,schema_e,new_oplog_data_set,oplog_data_object_id,oplog_data_ns,tables_mappings,database_name,schema_name))
+        else:
+            if not is_root_object_updated:
+                ret_val.extend (update_cmd(dbreq,schema_e,oplog_data_set,oplog_data_object_id,oplog_data_ns,tables_mappings,database_name,schema_name))
+            is_root_object_updated = True
+    return ret_val
+
+def update_list (dbreq, schema_e, upd_path_str, oplog_data_set, oplog_data_object_id, database_name, schema_name):
+    if type(schema_e) != dict:
+        schema = schema_e.schema
+    else:
+        schema = schema_e
+    ret_val = []
+    doc_id = get_obj_id_recursive(oplog_data_object_id, [], [])
+    del_stmnt = delete(dbreq, schema, upd_path_str, doc_id.itervalues().next(), database_name, schema_name)
+    for op in del_stmnt:
+        if type(del_stmnt[op]) is dict:
+            for k in del_stmnt[op]:
+                ret_val.append({k:[tuple(del_stmnt[op][k])]})
+
+    tables, initial_indexes \
+        = get_tables_data_from_oplog_set_command(schema_e,
+                                                 oplog_data_set,
+                                                 oplog_data_object_id)
+    for name, table in tables.iteritems():
+        rr = generate_insert_queries(table, schema_name, "", initial_indexes)
+
+        ret_val.append({rr[0]:rr[1]})
+    return  ret_val
+
+def update_cmd (dbreq, schema_e, oplog_data_set, oplog_data_object_id, oplog_data_ns, tables_mappings, database_name, schema_name):
     #compatibility with schema object for insert
     if type(schema_e) != dict:
         schema = schema_e.schema
     else:
         schema = schema_e
-    doc_id = get_obj_id(oplog_data)
-    u_data = oplog_data["o"]["$set"]
-    root_table_name = oplog_data["ns"].split('.')[-1]
-    collection_name = oplog_data["ns"]
+
+    doc_id = get_obj_id_recursive(oplog_data_object_id, [], [])
+    u_data = oplog_data_set
+    root_table_name = oplog_data_ns.split('.')[-1]
+    ret_val = []
+
     k = u_data.iterkeys().next()
     updating_obj = k.split('.')
-    # simple object update
     upd_path = [root_table_name] + updating_obj
     upd_path_str = '.'.join(upd_path)
     ins_stmnt = {}
     del_stmnt = {}
     upd_stmnt = {}
-    tables_mappings = get_tables_structure(schema,root_table_name,{},{},'')
-    if not updating_obj[-1].isdigit():
-        # update array object.In that case we need to use two combine of two operations
-        # delete old array (all elements linked to parent record) and insert new one (all elements listed in oplog query)
-        if type(u_data[k]) is list:
-            del_stmnt = delete(dbreq, schema, upd_path_str, doc_id.itervalues().next(), database_name, schema_name)
 
-            tables, initial_indexes \
-                = get_tables_data_from_oplog_set_command(schema_e,
-                                                     oplog_data['o']['$set'],
-                                                     oplog_data['o2'])
-            res = []
-            for name, table in tables.iteritems():
-                rr = generate_insert_queries(table, schema_name, "", initial_indexes)
-                ins_stmnt[rr[0]] = rr[1]
+    if not updating_obj[-1].isdigit():
+        if type(u_data[k]) is list:
+            ret_val.extend(update_list(dbreq,schema_e, upd_path_str, oplog_data_set, oplog_data_object_id,database_name,schema_name))
         else:
-            #update root object just simple update needed
             id_column = doc_id.iterkeys().next()
             unfiltered_q_columns = get_query_columns_with_nested(schema, u_data, '', {})
             q_columns = {}
             for column in unfiltered_q_columns:
-                if column in tables_mappings[root_table_name].keys():
-                    q_columns[column] = unfiltered_q_columns[column]
-            q_statements_list = [ ('{column}=(%s)' if not get_quotes_using(schema,root_table_name,col,root_table_name) else '{column}=(%s)').format(column=col) for col in q_columns]
-            q_conditions = ('{column}=(%s)' if not get_quotes_using(schema,root_table_name,id_column,root_table_name) else '{column}=(%s)').format(column = id_column)
-            upd_statement_template = UPDATE_TMPLT.format( table = get_table_name_schema([database_name, schema_name,  root_table_name]), statements = ', '.join(q_statements_list), conditions = q_conditions)
+                updated_obj_split = column.split('.')
+                if updated_obj_split[-1] in tables_mappings[root_table_name].keys():
+                    q_columns[updated_obj_split[-1]] = unfiltered_q_columns[updated_obj_split[-1]]
+                else:
+                    if column in tables_mappings[root_table_name].keys():
+                        q_columns[column] = unfiltered_q_columns[column]
+
+            q_statements_list = [('{column}=(%s)' if not get_quotes_using(schema, root_table_name, col,
+                                                                          root_table_name) else '{column}=(%s)').format(
+                column=col) for col in q_columns]
+            q_conditions = ('{column}=(%s)' if not get_quotes_using(schema, root_table_name, id_column,
+                                                                    root_table_name) else '{column}=(%s)').format(
+                column=id_column)
+            upd_statement_template = UPDATE_TMPLT.format(
+                table=get_table_name_schema([database_name, schema_name, root_table_name]),
+                statements=', '.join(q_statements_list), conditions=q_conditions)
             upd_values = [q_columns[col] for col in q_columns] + [doc_id.itervalues().next()]
-            upd_stmnt[upd_statement_template] = upd_values
+            ret_val.append({upd_statement_template:[tuple(upd_values)]})
     else:
         # update nested element
         if type(u_data[k]) is dict:
 
-            # insert statement should to be added  in case when updated record  does not exist
-            # we should execute only one of the insert or update statements
+
+            for element in u_data[k]:
+                if not localte_in_schema(schema, updating_obj + [element]):
+                    continue
+                if type(u_data[k][element]) is list:
+                    oplog_data_set_list = {}
+                    upd_path_str = '.'.join([upd_path_str, element])
+                    upd_list_result = update_list(dbreq, schema_e,upd_path_str, {'.'.join([k, element]):u_data[k][element]},oplog_data_object_id,database_name,schema_name)
+                    for q_element in upd_list_result:
+                        ret_val.append(q_element)
+
             target_table_name = get_table_name_from_list(upd_path)
             q_conditions = get_conditions_list(schema, '.'.join(upd_path), doc_id.itervalues().next())
-            q_columns = get_query_columns_with_nested(schema, u_data[k], '', {})
-            # q_statements_str = ', '.join([('{column}=(%s)' if not get_quotes_using(schema,target_table_name,col,root_table_name) else '{column}=(%s)').format(column=col) for col in q_columns])
-            # q_conditions_str = ' and '.join([('{column}=(%s)' if not get_quotes_using(schema,target_table_name,col,root_table_name) else '{column}=(%s)').format(column = col) for col in q_conditions['target']])
+            q_columns_unfiltered = get_query_columns_with_nested(schema, u_data[k], '', {})
+            q_columns= {}
+            for it in q_columns_unfiltered:
+                if it in tables_mappings[target_table_name].keys():
+                    q_columns[it] = q_columns_unfiltered[it]
+            if len(q_columns) == 0:
+                return ret_val
             q_statements_str = ', '.join(['{column}=(%s)'.format(column=col) for col in q_columns])
-            q_conditions_str = ' and '.join(['{column}=(%s)'.format(column = col) for col in q_conditions['target']])
-            upd_statement_template = UPDATE_TMPLT.format( table=get_table_name_schema([database_name, schema_name,  target_table_name]), statements=q_statements_str, conditions=q_conditions_str)
-            upd_values = [q_columns[col] for col in q_columns] + [q_conditions['target'][col] for col in q_conditions['target']]
-            # INSERT
+            q_conditions_str = ' and '.join(['{column}=(%s)'.format(column=col) for col in q_conditions['target']])
+            upd_statement_template = UPDATE_TMPLT.format(
+                table=get_table_name_schema([database_name, schema_name, target_table_name]),
+                statements=q_statements_str, conditions=q_conditions_str)
+            upd_values = [q_columns[col] for col in q_columns] + [q_conditions['target'][col] for col in
+                                                                  q_conditions['target']]
             tables, initial_indexes \
                 = get_tables_data_from_oplog_set_command(schema_e,
-                                                     oplog_data['o']['$set'],
-                                                     oplog_data['o2'])
+                                                         oplog_data_set,
+                                                         oplog_data_object_id)
+
             for name, table in tables.iteritems():
                 rr = generate_insert_queries(table, schema_name, "", initial_indexes)
                 ins_stmnt[rr[0]] = rr[1]
-            #TODO miltiple queries in case of enclosed objects
+            # TODO miltiple queries in case of enclosed objects
             insert_statement_template = ins_stmnt.iterkeys().next()
-            upsert_statement_template = UPSERT_TMLPT.format(update=upd_statement_template, insert=insert_statement_template)
+            upsert_statement_template = UPSERT_TMLPT.format(update=upd_statement_template,
+                                                            insert=insert_statement_template)
             ins_values = list(ins_stmnt[insert_statement_template][0])
             upsert_values = upd_values + ins_values
             upd_stmnt[upsert_statement_template] = upsert_values
-            ins_stmnt = {}
-    ret_val = []
-    #TODO need to be fixed. result of delete opertion should be just single dictionary, where: keys - SQL template, values - values for template (data, conditions, ids)
-    for op in del_stmnt:
-        if type(del_stmnt[op]) is dict:
-            for k in del_stmnt[op]:
-                ret_val.append({k:[tuple(del_stmnt[op][k])]})
-    for op in upd_stmnt:
-        ret_val.append({op:[tuple(upd_stmnt[op])]})
-    for op in ins_stmnt:
-        ret_val.append({op:ins_stmnt[op]})
+
+            ret_val.append({upsert_statement_template:[tuple(upsert_values)]})
     return ret_val
 
 
@@ -143,6 +236,9 @@ def get_query_columns_with_nested (schema, u_data, parent_path, columns_list):
             column_name = get_field_name_without_underscore(k)
         if type(u_data[k]) is dict:
             get_query_columns_with_nested(schema, u_data[k], column_name, columns_list).copy()
+        # if type(u_data[k]) in list:
+        #     print('update list {0}'.format(parent_path) )
+        #     pass
         if type(u_data[k]) == bson.objectid.ObjectId:
             columns_list[column_name+'_oid'] = str(u_data[k])
         else:
