77ed347e6e01f3668799c2e471b3be5b091d528a
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2016-07-08 17:11:02 -0500

    load str data from psql as unicode

--- test_data/oplog1/after_collection_posts.js
+++ test_data/oplog1/after_collection_posts.js
@@ -2,7 +2,7 @@
  {
      "_id": { "$oid": "56b8f05cf9fcee1b00000010" },
      "title": "sada",
-     "body": "body3",
+     "body": "body3 \u0444\u044b\u0432\u0430",
      "user_id": "56b8d7caf9fcee1b00000001",
      "updated_at": { "$date" : "2016-02-08T20:02:12.985Z"},
      "created_at": { "$date" : "2016-02-08T19:45:32.501Z"}
