85e18152d9ced6d764dc50edecf80177f1b03313
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2016-07-13 10:44:34 -0500

    added forgotten oplog part 2

---
+++ test_data/oplog2/oplog_simulate_added_after_initload.js
@@ -0,0 +1,15 @@
+[
+{
+  "ts": {"$timestamp": {"t": 1994278289, "i": 4}},
+  "op": "u",
+  "ns": "rails4_mongoid_development.guests",
+  "o2": {
+    "id": 13
+  },
+  "o": {
+    "$set": {
+      "comment": "foo-foo"
+    }
+  }
+}
+]
