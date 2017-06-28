6b936a78244d442ab1f6628434670d2a35e6271a
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2017-06-28 12:08:20 -0500

    yet another travis attemp

--- run_cov_tests.sh
+++ run_cov_tests.sh
@@ -1,3 +1,4 @@
+export TEST_PSQLCONN="dbname=etl user=etl password=etl"
 export PYTHONPATH=$PYTHONPATH:$PWD
 py.test --cov-report=term-missing --cov=gizer tests/
 py.test --cov-report=term-missing --cov=mongo_schema ../mongo_schema tests/
